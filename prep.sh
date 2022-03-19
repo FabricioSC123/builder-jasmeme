c4e7f01ff2d199788cb68dda4d9b2cd50696e491
FabricioSC123 <fabriciosalaspaypal@gmail.com>
2022-03-19 14:43:49 -0500

    jasmeme: aria2c default

--- prep.sh
+++ prep.sh
@@ -1,2 +1,2 @@
 cd ~
-sudo apt -y install cpio brotli simg2img abootimg git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip zip screen attr ccache libssl-dev imagemagick schedtool
+sudo apt -y install cpio brotli aria2c simg2img abootimg git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip zip screen attr ccache libssl-dev imagemagick schedtool
